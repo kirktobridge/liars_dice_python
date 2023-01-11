2ebc599feea209937a7512af099f91aea02bd664
kirktobridge <isaacsnc@gmail.com>
2023-01-11 12:17:21 -0500

    took notes on risk for decision making

--- Player.py
+++ Player.py
@@ -28,7 +28,7 @@ class Player:
         self.wild_count = 0
         self.mode_count = 0
         self.spot = spot
-        self.addl_guessed_cnt = 0
+        self.risk_appetite = 0
         self.bid = []
 
     def lose_die(self):
@@ -128,10 +128,10 @@ class Player:
         # Use this information, along with the number of other players' dice,
         # to calculate the probability of the previous bid being true. This can be done
         # using a scipy function to calculate the binomial cumulative probability.
-
         new_action = Constants.ACTIONS[5]
         self.rolls_mode = mode(self.dice)  # what is our most common roll?
         self.mode_count = self.dice.count(self.rolls_mode) + self.count_ones()
+
         # If we are the first player
         if prev_action == Constants.ACTIONS[0]:
             # we have a safe bid (mode)
@@ -140,16 +140,11 @@ class Player:
             else:  # we have no mode assuming constant is 2
                 output = [Constants.MINIMUM_BID,
                           self.dice[random.randint(0, self.num_dice-1)]]
-                # TODO
-
-            # TODO what behavior should we instigate if we are the first player?
-            # we have to bid, can't challenge or spot on
-            # self.bid()
-
             new_action = Constants.ACTIONS[1]
+
         # If the previous player made a bid
         elif prev_action == (Constants.ACTIONS[1] or Constants.ACTIONS[2]):
-            return [[-1, -1], Constants.ACTIONS[5] + ' TODO BID/RAISE/CHALLENGE DECISION', self.name]
+            # return [[-1, -1], Constants.ACTIONS[5] + ' TODO BID/RAISE/CHALLENGE DECISION', self.name]
             model = binom(n=tot_other_dice, p=2/6)  # set up binomial model
             # ns and 1s count as ns
             prev_bid = prev_event[0]  # pulls previous turn's bid
@@ -181,20 +176,25 @@ class Player:
                 # p(x >= y) = 1 - p(x =< y-1)
                 cumulative_probability = 1 - model.cdf(needed_cnt-1)
                 spot_on_probability = model.pmf(needed_cnt)
-                # TODO compare these probabilities and decide if we should 'spot on'
+                # for all other faces besides the one in the previous bid:
+                # check the probability of
+                # TODO compare these probabilities and decide if we should 'spot on', raise or challenge
                 # TODO compare these probabilities to probability of each possible raised/face changed bid
-                # TODO get list of previous bids' faces from prev_events array (where action = bid/raise)
                 # may need to pass all previous events instead of just the one previous event...
-                # TODO what do we do when nothing we want to say or can say is likely?
+                # TODO what do we do when nothing we want to say or can say is likely? randomize choice, but:
+                # get list of previous bids' faces from prev_events array (where action = bid/raise)
+                # favor bidding on these faces if we can
+                # and perhaps favor actions with riskier consequences (challenge, spot on) based on a
+                # risk personality attribute
 
+        # should the player guess if the previous player was lying/taking bad risk
+        #  based on how many dice they have and how many dice they bet on?
         #
         # (3) Decision Making: Evaluate stats and make decision
         #
         # TODO: bid new face, raise bid, challenge, or spot on
             # if previous bid unlikely, challenge
-            if Player.grade(cumulative_probability) == 'LOW':
-                new_action = Constants.ACTIONS[3]
-                output = [-1, -1]
+
         else:
             output = [-1, -1]
             new_action = Constants.ACTIONS[5] + \
