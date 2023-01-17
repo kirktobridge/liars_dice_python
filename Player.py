6029a63ebb1544113a1ce69fb2935fa37b81d78f
kirktobridge <isaacsnc@gmail.com>
2023-01-17 11:10:57 -0500

    added distributions for player behavior (risk/etc)

--- Player.py
+++ Player.py
@@ -12,7 +12,7 @@ from scipy.stats import binom
 
 class Player:
 
-    def __init__(self, name, spot='CPU', eliminated=False, num_dice=Constants.MAX_NUM_DICE):
+    def __init__(self, name: str, spot='CPU', eliminated=False, num_dice=Constants.MAX_NUM_DICE):
         '''Constructor for the Player object. Initializes key variables.'''
         self.name = name
         if Constants.DEBUG:
@@ -28,8 +28,10 @@ class Player:
         self.wild_count = 0
         self.mode_count = 0
         self.spot = spot
-        self.risk_appetite = random.randint(1, 5)  # TODO)
-        self.peer_pressure_score = random.randint(1, 5)
+        self.risk_appetite = random.choice(
+            Constants.RISK_APPETITE_DISTRIBUTION)
+        self.peer_pressure_score = random.choice(
+            Constants.PEER_PRESSURE_DISTRIBUTION)
         self.bid = []
 
     def lose_die(self):
@@ -65,7 +67,7 @@ class Player:
         # ie each Player will have slightly different risk tolerance levels
         return grade
 
-    def bid(self, tot_other_dice):
+    def bid(self, tot_other_dice: int):
         '''Allows human user to bid.'''
         bid_count = None
         bid_face = None
@@ -109,7 +111,7 @@ class Player:
         else:
             raise Exception('CPUs should not be using bid() function')
 
-    def take_turn(self, prev_events, tot_other_dice):
+    def take_turn(self, prev_events: deque, tot_other_dice: int):
         # TODO form and react to impressions of other players (trust score, expected bids, expected count of ones based on bids)
         # for each turn record
 
@@ -139,9 +141,10 @@ class Player:
         if prev_action == Constants.ACTIONS[0]:
             # if we have a safe bid (mode)
             if self.mode_count >= Constants.MINIMUM_BID:
-                output = [Constants.MINIMUM_BID, self.rolls_mode]
+                output = [Constants.MINIMUM_BID +
+                          self.risk_appetite, self.rolls_mode]
             else:  # we have no mode assuming constant is 2
-                output = [Constants.MINIMUM_BID,
+                output = [Constants.MINIMUM_BID + self.risk_appetite-1,
                           self.dice[random.randint(0, self.num_dice-1)]]
             new_action = Constants.ACTIONS[1]
 
@@ -158,8 +161,8 @@ class Player:
             # needed count is how many dice with the desired face we need for the previous bid
             # to be true, factoring in the roll we already know the outcome for (ours)
             if prev_bid_needed_cnt == 0:
-                cumulative_probability = 1.0
-                # TODO evaluate spot on conditions?
+                challenge_success_probability = 0
+                # if we know this bid is true, don't challenge
             elif prev_bid_needed_cnt < 0:
                 # negative number means we can safely raise this bid
                 # current philosophy for raising is that we don't want to
@@ -170,6 +173,7 @@ class Player:
                 # plus a few risked dice. This risk addition could be a personality
                 # attribute of the Player object, perhaps something to randomize (or
                 # create a distribution of across the playerset for a game)
+                challenge_success_probability = 0.0
                 spot_on_probability = 0.0
                 output = [prev_bid_cnt+1, prev_bid_face]
                 new_action = Constants.ACTION[2]
@@ -187,8 +191,8 @@ class Player:
 
                 # (1) GET PROBABILITY OF PREVIOUS BID - CHALLENGE
                 #   Lower score means we may consider challenge.
-                cumulative_probability = 1 - \
-                    model.cdf(prev_bid_needed_cnt-1)
+                challenge_success_probability = model.cdf(
+                    prev_bid_needed_cnt-1)
                 # TODO are we using the right function?
                 # (2) GET PROBABILITY OF PREVIOUS BID - SPOT ON
                 #   Higher score means we may consider calling 'spot on.'
@@ -224,7 +228,6 @@ class Player:
                         # TODO is this the right function?
                     elif needed_cnt <= 0:
                         bid_probability = 1.0
-                   # TODO do we need: if bid_probability > best_bid_probability:
                     risk_ranking.append([bid_probability, legal_bid])
                 risk_ranking.sort(key=lambda x: x[0], reverse=True)
                 # favor popular faces? persuadability randomized personality attribute??
@@ -234,16 +237,22 @@ class Player:
                     prob for prob in risk_ranking if prob[0] == best_bid_probability]
                 if len(best_bids) > 1:
                     # if we are peer pressure sensitive, pick most common
-                    if self.risk_appetite == 5:
+                    if self.peer_pressure_score >= 5:
                         all_prev_bids_faces = [bid[1]
                                                for all_prev_bid in all_prev_bids]
                         prev_bids_face_mode = mode(all_prev_bids_faces)
-                        best_bid_candidate = None
+                        best_bid_candidate = 0
                         for bid in best_bids:
-                            if bid[]
-                        pass
+                            if bid[1] == prev_bids_face_mode:
+                                best_bid = bid
+                                break
+                            else:
+                                continue
+                    # otherwise just take the top bid
                     else:
                         best_bid = best_bids[0]
+                else:
+                    best_bid = best_bids[0]
                 # TODO compare these probabilities and decide if we should 'spot on', raise/match,
                 #  or challenge
                 # TODO what do we do when nothing we want to say or can say is likely? randomize choice, but:
@@ -273,7 +282,7 @@ class Player:
         #
         return [output, new_action, self.name]
 
-    def get_needed_cnt(self, bid):
+    def get_needed_cnt(self, bid: list[int]):
         '''Produces the number of rolled faces needed for a bid to be true,
         after including the ones we have.'''
         bid_cnt = bid[0]
@@ -283,7 +292,7 @@ class Player:
         needed_cnt = bid_cnt - face_self_match_cnt
         return needed_cnt
 
-    def challenge(self, p):
+    def challenge(self):
         if self.spot == 'CPU':
             pass  # TODO AI behavior
         elif self.spot == 'HUMAN':
